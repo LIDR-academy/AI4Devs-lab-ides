3743ee893514eb80a313f2f5dfa013d8bbd646a4
jose miguel <richard65838@gmail.com>
2025-03-09 22:55:44 -0300

    dashboard

---
+++ frontend/src/pages/RecluterDashboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const RecluterDashboard: React.FC = () => {
+    return (
+        <div className="container mt-4">
+            <h1 className="mb-4">Recluter Dashboard</h1>
+            <button className="btn btn-primary">Agregar Candidato</button>
+        </div>
+    );
+};
+
+export default RecluterDashboard; 
